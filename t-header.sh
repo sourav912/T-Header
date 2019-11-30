119ec2d21b3ce6bf4db1c74d31d5ff9be21b3922
sourav912 <57267192+sourav912@users.noreply.github.com>
2019-11-30 10:35:36 +0600

    Update t-header.sh

--- t-header.sh
+++ t-header.sh
@@ -85,10 +85,10 @@ fi
 done
 clear
 TNAME="$PROC";
-echo -e "\033[32m$(figlet -f Remo773.flf "$TNAME")\033[0m";
+echo -e "\033[32m$(figlet -f Remo773.flf "$Black")\033[0m";
 echo "";
 echo -e '\e[0;35m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
-echo -e '\033[1;43;30m### SUBSCRIBE MY YOUTUBE CHANNEL ### \033[0m';
+echo -e '\033[1;43;30m### All Hacking Tool SJ ### \033[0m';
 echo -e '\e[0;35m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
 echo "";
 echo -e "\e[1;34m┌─[\e[1;32m$PROC\e[1;33m@\e[36mtermux\e[1;34m]\e[0m-\e[1;34m[\e[33m$(date +'%d %a %b')\e[1;34m]\e[0m-\e[1;34m[\e[33m$(date +'%l:%M %p')\e[1;34m]
